42f054cb608d68523b7d4ca3262613cc2b005f11
Lucas Moura <kl.lucasmoura@gmail.com>
2025-05-19 11:37:18 -0300

    Funcionalidade de cadastro de produtos no banco de dados

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -13,7 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -23,12 +23,22 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)";
+        try
+        (PreparedStatement prep = conn.prepareStatement(sql)){
+        prep.setString(1, produto.getNome());
+        prep.setDouble(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        }
         
         
-        //conn = new conectaDAO().connectDB();
         
-        
-    }
+    
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -22,7 +22,8 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=@Lucas3770");
+            System.out.println("CONECTADO!");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
